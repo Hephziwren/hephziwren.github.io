4cdd58ee84622b11d2abb8e68313e97d1da3addb
Felix Waller <felixwaller.public@gmail.com>
2022-04-13 19:48:30 +0100

    Add dialog heading

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const imgs = [
     { src: "./assets/278216765_996998341021926_6258374670432842178_n.jpg", title: "Picture", desc: "A long description of the art A long description of the art A long description of the art " },
-    { src: "./assets/278227793_4947074635390430_4753790350343935391_n.jpg", title: "Picture", desc: "A short description of the art" },
+    { src: "./assets/278227793_4947074635390430_4753790350343935391_n.jpg", title: "Picture", desc: "" },
     { src: "./assets/278284031_110187734877623_5199219830527013741_n.jpg", title: "Picture", desc: "A short description of the art" },
     { src: "./assets/278301137_161553479581742_4761664630371862016_n.jpg", title: "Picture", desc: "A short description of the art" },
     { src: "./assets/278306877_478874850604983_4254870354216129447_n.jpg", title: "Picture", desc: "A short description of the art" },
@@ -20,6 +20,7 @@ const imgs = [
 const columns = document.getElementsByClassName("column");
 let dialog = document.getElementById("dialog");
 let dialogImg = document.getElementById("dialog__img");
+let dialogH3 = document.getElementById("dialog__h3");
 let dialogP = document.getElementById("dialog__p");
 
 for (let i = 0; i < imgs.length; i++) {
@@ -34,7 +35,9 @@ for (let i = 0; i < imgs.length; i++) {
 document.getElementById("dialog__button").addEventListener("click", e => dialog.close());
 
 function showDialog(e) {
+    let index = e.target.dataset.index;
     dialogImg.src = e.target.src;
-    dialogP.textContent = imgs[e.target.dataset.index].desc;
+    dialogH3.textContent = imgs[index].title;
+    dialogP.textContent = imgs[index].desc;
     dialog.showModal();
 }
